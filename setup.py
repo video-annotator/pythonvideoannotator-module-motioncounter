42e4be18f0386ccb1801bfb680fa54e12d84b3a9
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-10-31 16:33:01 +0000

    fix version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import re
 
 setup(
 	name='Python video annotator - module - motion counter',
-	version=0.0.1,
+	version="0.0.1",
 	description="""""",
 	author=['Ricardo Ribeiro'],
 	author_email='ricardojvr@gmail.com',
